371e364f92e78f0f74e306bbba5c23849e82ce69
denis <yudaev1@gmail.com>
2021-07-14 10:35:11 +0300

    Implement download

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,6 +1,8 @@
 import requests
 from urllib.parse import urlparse
 import re
+from pathlib import Path
+from os.path import join
 
 def get_file(url, file_path):
     response = requests.get(url, stream=True)
@@ -11,11 +13,12 @@ def get_file(url, file_path):
 
 
 def download(url, output_path):
-
+    directory = Path(output_path)
+    if not directory.is_dir():
+        raise FileNotFoundError
     parsed_url = urlparse(url)
-    url_path = f'{parsed_url.netloc}{parsed_url.path}'
+    url_path = join(parsed_url.netloc, parsed_url.path)
     formatted_url_path = re.sub(r'[^0-9a-z-.]', '-', url_path)
-
     file_path = f'{output_path}/{formatted_url_path}{".html"}'
     get_file(url, file_path)
 
