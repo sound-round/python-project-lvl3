e25bc08b69c8a06790637f8054f0601df80f56e2
denis <yudaev1@gmail.com>
2021-07-15 18:02:55 +0300

    Update page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -3,7 +3,11 @@ from urllib.parse import urlparse
 import re
 from pathlib import Path
 import os
-from os.path import splitext
+from os.path import splitext, join
+from bs4 import BeautifulSoup
+
+
+FORBIDDEN_CHARS = r'[^0-9a-zA-Z-]'
 
 
 def download_html(url, file_path, client):
@@ -21,8 +25,26 @@ def create_download_dir(html_path):
         os.makedirs(dir_path)
     return dir_path
 
-def download_images(html_path, dir_path):
-    pass
+
+def download_images(html_path, dir_path, domain_name, netloc):
+    html_file = open(html_path, 'r')
+    parsed_html = BeautifulSoup(html_file, 'html.parser')
+    image_links = parsed_html.find_all('img')
+    for link in image_links:
+        source = link.get('src')
+        url = join(domain_name, source)
+        image_name = join(netloc, source)
+        formatted_image_name = re.sub(FORBIDDEN_CHARS, '-', image_name).lower()
+        image_path = join(dir_path, formatted_image_name)
+
+        response = requests.get(url, stream=True)
+
+        with open(image_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+        # TODO change html.
+        
+
 
 
 def download(url, output_path=os.getcwd(), library=requests):
@@ -30,10 +52,13 @@ def download(url, output_path=os.getcwd(), library=requests):
     if not directory.is_dir():
         raise FileNotFoundError
     parsed_url = urlparse(url)
-    file_name = f'{parsed_url.netloc}{parsed_url.path}'
-    formatted_file_name = re.sub(r'[^0-9a-zA-Z-]', '-', file_name).lower()
-    file_path = f'{output_path}/{formatted_file_name}{".html"}'
-    download_html(url, file_path, library)
-    download_dir_path = create_download_dir(file_path)
-    download_images(file_path, download_dir_path)
-    return file_path
+    netloc = parsed_url.netloc
+    html_name = f'{netloc}{parsed_url.path}'
+    formatted_html_name = re.sub(FORBIDDEN_CHARS, '-', html_name).lower()
+    html_path = f'{output_path}/{formatted_html_name}{".html"}'
+    download_html(url, html_path, library)
+    download_dir_path = create_download_dir(html_path)
+    domain_name = join(parsed_url.scheme, parsed_url.netloc)
+    download_images(html_path, download_dir_path, domain_name, netloc)
+
+    return html_path
