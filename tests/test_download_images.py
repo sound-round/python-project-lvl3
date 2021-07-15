89d4a52be1dfd3753f90b220b140c7173363aee6
denis <yudaev1@gmail.com>
2021-07-15 18:03:23 +0300

    Update test_download_images.py

--- tests/test_download_images.py
+++ tests/test_download_images.py
@@ -1,18 +1,21 @@
 import pytest
 import tempfile
 from page_loader.page_loader import download_images
-from os.path import join, split
+from os.path import split
 import re
 from tests.test_download import FORBIDDEN_CHARS
 from pathlib import Path
 
 
 HTML_PATH = 'tests/fixtures/badcode-ru-docker-fixture.html'
+# TODO this:
+domain_name = ''
+netloc = ''
 
 
 def test_download_images():
     with tempfile.TemporaryDirectory() as tmp_directory:
-        images_paths = download_images(HTML_PATH, tmp_directory)
+        images_paths = download_images(HTML_PATH, tmp_directory, domain_name, netloc)
         for image_path in images_paths:
             image = Path(image_path)
             image_name = split(image_path)[-1]
@@ -20,6 +23,7 @@ def test_download_images():
             assert image.is_file()
             assert not re.search(FORBIDDEN_CHARS, image_name)
             assert not ('https---' in image_path)
+            assert domain_name is not None
 
 
 def test_download_images_exceptions():
