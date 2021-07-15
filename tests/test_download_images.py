099ec3038275246a29cba97e555f4460d3219df4
denis <yudaev1@gmail.com>
2021-07-15 16:24:56 +0300

    Create test_download_images.py

---
+++ tests/test_download_images.py
@@ -0,0 +1,32 @@
+import pytest
+import tempfile
+from page_loader.page_loader import download_images
+from os.path import join, split
+import re
+from tests.test_download import FORBIDDEN_CHARS
+from pathlib import Path
+
+
+HTML_PATH = 'tests/fixtures/badcode-ru-docker-fixture.html'
+
+
+def test_download_images():
+    with tempfile.TemporaryDirectory() as tmp_directory:
+        images_paths = download_images(HTML_PATH, tmp_directory)
+        for image_path in images_paths:
+            image = Path(image_path)
+            image_name = split(image_path)[-1]
+            assert isinstance(image_path, str)
+            assert image.is_file()
+            assert not re.search(FORBIDDEN_CHARS, image_name)
+            assert not ('https---' in image_path)
+
+
+def test_download_images_exceptions():
+    # TODO discuss with the mentor
+    with tempfile.TemporaryDirectory() as tmp_directory:
+        with pytest.raises(ValueError):
+            download_images('', tmp_directory)
+
+    with pytest.raises(FileNotFoundError):
+        download_images(HTML_PATH, '/undefined')
