28e2e259d506ac94aa9611e016d91632e0b9f08b
denis <yudaev1@gmail.com>
2021-07-15 16:35:48 +0300

    Implement create_download_dir

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -3,15 +3,26 @@ from urllib.parse import urlparse
 import re
 from pathlib import Path
 import os
-# from os.path import join
+from os.path import splitext
 
 
-def download_file(url, file_path, client):
+def download_html(url, file_path, client):
     response = client.get(url, stream=True)
 
     with open(file_path, 'wb') as file:
         for chunk in response.iter_content(chunk_size=128):
             file.write(chunk)
+
+
+def create_download_dir(html_path):
+    root, _ = splitext(html_path)
+    dir_path = root + '_files'
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    return dir_path
+
+def download_images(html_path, dir_path):
+    pass
 
 
 def download(url, output_path=os.getcwd(), library=requests):
@@ -22,6 +33,7 @@ def download(url, output_path=os.getcwd(), library=requests):
     file_name = f'{parsed_url.netloc}{parsed_url.path}'
     formatted_file_name = re.sub(r'[^0-9a-zA-Z-]', '-', file_name).lower()
     file_path = f'{output_path}/{formatted_file_name}{".html"}'
-    download_file(url, file_path, library)
-
+    download_html(url, file_path, library)
+    download_dir_path = create_download_dir(file_path)
+    download_images(file_path, download_dir_path)
     return file_path
