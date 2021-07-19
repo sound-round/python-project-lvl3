ec41c90c29f5cb3eba22b094e185bd879486fced
denis <yudaev1@gmail.com>
2021-07-19 16:01:51 +0300

    Update page-loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 import re
 from pathlib import Path
 import os
-from os.path import splitext, join
+from os.path import split, splitext, join
 from bs4 import BeautifulSoup
 
 
@@ -27,24 +27,39 @@ def create_download_dir(html_path):
 
 
 def download_images(html_path, dir_path, domain_name, netloc):
+    directory = Path(dir_path)
+    if not directory.is_dir():
+        raise FileNotFoundError
+    file_path = Path(html_path)
+    if not file_path:
+        raise FileNotFoundError
     html_file = open(html_path, 'r')
     parsed_html = BeautifulSoup(html_file, 'html.parser')
     image_links = parsed_html.find_all('img')
+    dir_name = split(dir_path)[1]
+    paths = []
     for link in image_links:
         source = link.get('src')
-        url = join(domain_name, source)
-        image_name = join(netloc, source)
+        # url = join(domain_name, source)
+        url = f'{domain_name}/{source}'
+        image_name, ext = splitext(f'{netloc}/{source}')
         formatted_image_name = re.sub(FORBIDDEN_CHARS, '-', image_name).lower()
-        image_path = join(dir_path, formatted_image_name)
-
+        image_path = join(dir_path, formatted_image_name) + ext
+        paths.append(image_path)
         response = requests.get(url, stream=True)
 
         with open(image_path, 'wb') as file:
             for chunk in response.iter_content(chunk_size=128):
                 file.write(chunk)
         # TODO change html.
-        
+        source_path = join(dir_name, formatted_image_name) + ext
+        link['src'] = source_path
+    html_file.close()
 
+    with open(html_path, 'wb') as file:
+        file.write(parsed_html.encode(formatter="html5"))
+
+    return paths
 
 
 def download(url, output_path=os.getcwd(), library=requests):
@@ -58,7 +73,7 @@ def download(url, output_path=os.getcwd(), library=requests):
     html_path = f'{output_path}/{formatted_html_name}{".html"}'
     download_html(url, html_path, library)
     download_dir_path = create_download_dir(html_path)
-    domain_name = join(parsed_url.scheme, parsed_url.netloc)
+    domain_name = f'{parsed_url.scheme}://{parsed_url.netloc}'
     download_images(html_path, download_dir_path, domain_name, netloc)
 
     return html_path
