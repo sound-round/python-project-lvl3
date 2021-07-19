4ff70ef01edf2f45fa75bc9b7056788e01ecd9e4
denis <yudaev1@gmail.com>
2021-07-19 17:41:56 +0300

    Update test_download_image.py

--- tests/test_download_images.py
+++ tests/test_download_images.py
@@ -1,21 +1,32 @@
 import pytest
 import tempfile
 from page_loader.page_loader import download_images
-from os.path import split
+from os.path import split, join
 import re
 from tests.test_download import FORBIDDEN_CHARS
 from pathlib import Path
+import requests_mock
+import requests
+from shutil import copyfile
+import filecmp
 
-
+URL = 'https://badcode.ru'
 HTML_PATH = 'tests/fixtures/badcode-ru-docker-fixture.html'
 # TODO this:
-domain_name = ''
-netloc = ''
+netloc = 'badcode.ru'
+
+
+def test_download_images_mock(requests_mock):
+    requests_mock.get(URL, text='data')
+    assert 'data' == requests.get(URL).text
 
 
 def test_download_images():
     with tempfile.TemporaryDirectory() as tmp_directory:
-        images_paths = download_images(HTML_PATH, tmp_directory, domain_name, netloc)
+        copy_html_path = join(tmp_directory, split(HTML_PATH)[1])
+        copyfile(HTML_PATH, copy_html_path)
+        images_paths = download_images(copy_html_path, tmp_directory, URL, netloc)
+
         for image_path in images_paths:
             image = Path(image_path)
             image_name = split(image_path)[-1]
@@ -23,14 +34,14 @@ def test_download_images():
             assert image.is_file()
             assert not re.search(FORBIDDEN_CHARS, image_name)
             assert not ('https---' in image_path)
-            assert domain_name is not None
+        assert not filecmp.cmp(HTML_PATH, copy_html_path)  # TODO remake
 
 
 def test_download_images_exceptions():
     # TODO discuss with the mentor
     with tempfile.TemporaryDirectory() as tmp_directory:
-        with pytest.raises(ValueError):
-            download_images('', tmp_directory)
+        with pytest.raises(FileNotFoundError):
+            download_images('', tmp_directory, domain_name, netloc)
 
     with pytest.raises(FileNotFoundError):
-        download_images(HTML_PATH, '/undefined')
+        download_images(HTML_PATH, '/undefined', domain_name, netloc)
