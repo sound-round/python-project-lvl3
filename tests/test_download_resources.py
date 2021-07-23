21c29b241a559e42966341b912c8398c7efdd709
denis <yudaev1@gmail.com>
2021-07-23 11:10:52 +0300

    Update tests/

--- tests/test_download_resources.py
+++ tests/test_download_resources.py
@@ -15,6 +15,7 @@ URL = 'https://ru.hexlet.io'
 NETLOC = 'ru.hexlet.io'
 FIXTURES_PATH = 'fixtures/resources_download_test'
 HTML = 'ru-hexlet-io-courses.html'
+LOADING_DIR = 'ru-hexlet-io-courses_files'
 DATA = [
     ('/assets/application.css', 'application.css', 'ru-hexlet-io-assets-application.css'),
     ('/courses', 'courses', 'ru-hexlet-io-courses.html'),
@@ -22,10 +23,11 @@ DATA = [
     ('/packs/js/runtime.js', 'runtime.js', 'ru-hexlet-io-packs-js-runtime.js'),
 ]
 
-'''
-HTML_PATH_AFTER = (
-    'tests/fixtures/images_download_test/images-fixture-after.html'
-)'''
+
+def read(file_path):
+    with open(file_path, 'r') as f:
+        file = f.read()
+    return file
 
 
 def read_in_bytes(file_path):
@@ -42,14 +44,13 @@ def get_fixture_path(fixture_name):
     )
 
 
-def test_download_images_mock(requests_mock):
+def test_download_res_mock_to_del(requests_mock):
     requests_mock.get(URL, text='data')
     assert 'data' == requests.get(URL).text
 
 
-def test_resources_download_mocking(requests_mock):
+def test_download_resources(requests_mock):
     with tempfile.TemporaryDirectory() as tmp_directory:
-        #requests_mock.get(URL, content=read_in_bytes(HTML_PATH_BEFORE))
         for items in DATA:
             subpath, fixture, loaded_file = items
             fixture_path = get_fixture_path(fixture)
@@ -62,35 +63,22 @@ def test_resources_download_mocking(requests_mock):
         download_dir = create_download_dir(copy_html_path)
         download_resources(copy_html_path, download_dir, URL, NETLOC)
 
-        #html_after = BeautifulSoup(open(HTML_PATH_AFTER, 'r'), 'html.parser')
-        #html_result = BeautifulSoup(open(copy_html_path, 'r'), 'html.parser')
-        # find all "src" in both files
-        #image_links1 = html_after.find_all('img')
-        #image_links2 = html_result.find_all('img')
-        #src1 = []
-        #src2 = []
-        #for link in image_links1:
-        #    src1.append(link['src'])
-        #for link in image_links2:
-        #    src2.append(link['src'])
-
-        #assert src1 == src2
         for items in DATA:
             subpath, fixture, loaded_file = items
             fixture_path = get_fixture_path(fixture)
-            assert filecmp.cmp(
-                fixture_path,
-                f'{tmp_directory}/ru-hexlet-io-courses_files/{loaded_file}',
-                shallow=False
+            assert read_in_bytes(
+                fixture_path
+            ) == read_in_bytes(
+                f'{tmp_directory}/{LOADING_DIR}/{loaded_file}'
             )
-        assert filecmp.cmp(
-                get_fixture_path('after/ru-hexlet-io-courses.html'),
-                f'{tmp_directory}/ru-hexlet-io-courses.html',
-                shallow=False
+        assert read(
+            get_fixture_path(f'after/{HTML}')
+        ) == read(
+                f'{tmp_directory}/{HTML}'
             )
 
 
-def test_download_images_exceptions():
+def test_download_resources_exceptions():
     # TODO discuss with the mentor
     with tempfile.TemporaryDirectory() as tmp_directory:
         with pytest.raises(FileNotFoundError):
