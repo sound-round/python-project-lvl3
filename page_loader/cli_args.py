023e9abf3cb0836a48934c7125bed37aa8bc14a6
denis <yudaev1@gmail.com>
2021-08-04 13:09:09 +0300

    Update cli_args.py

--- page_loader/cli_args.py
+++ page_loader/cli_args.py
@@ -1,16 +1,26 @@
 from page_loader.logger import logging_info
 import argparse
 import os
+import pkg_resources
+
+
+version = pkg_resources.get_distribution('hexlet-code').version
 
 
 @logging_info('Parsing arguments')
 def parse():
-    parser = argparse.ArgumentParser(description='Download page')
-    parser.add_argument("url")
+    parser = argparse.ArgumentParser(description='Download the page')
+    parser.add_argument('url', help='url of the page')
+    parser.add_argument(
+        '-V', '--version',
+        help='output the version number',
+        action='version',
+        version=version
+    )
     parser.add_argument(
         '-o', '--output',
         default=os.getcwd(),
-        help='set output folder (default: current directory)',
+        help='set output folder (default: "/app")',
     )
 
     return parser.parse_args()
