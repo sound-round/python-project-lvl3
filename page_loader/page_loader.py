c8af1d549d2242b8987b53440f05df645ef04573
denis <yudaev1@gmail.com>
2021-08-11 10:55:05 +0300

    Update page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,7 +1,10 @@
 from page_loader.logger import logging_info
 from pathlib import Path
 from page_loader.resource_downloader import download_resources
-from page_loader.resource_downloader import get_path, download_file
+from page_loader.resource_downloader import download_file
+from urllib.parse import urlparse
+from os.path import join
+from page_loader.resource_downloader import format_name
 import os
 import logging
 import requests
@@ -10,16 +13,23 @@ import requests
 HTML_EXT = '.html'
 
 
+def get_html_path(url, output_path):
+    parsed_url = urlparse(url)
+    file_name = f'{parsed_url.netloc}{parsed_url.path}'
+    formatted_file_name = format_name(file_name)
+    return join(output_path, formatted_file_name) + HTML_EXT
+
+
 @logging_info('Downloading html')
-def download_html(url, file_path, client):
+def download_html(url, file_path, client=requests):
     logging.info('Requesting to %s', url)
     response = client.get(url, stream=True)
     response.raise_for_status()
-
+    logging.info(f'html_path: {file_path}')
     download_file(file_path, url, response)
 
 
-def download(url, output_path=os.getcwd(), library=requests):
+def download(url, output_path=os.getcwd()):  # TODO delete library
     if not url:
         raise ValueError
 
@@ -27,8 +37,8 @@ def download(url, output_path=os.getcwd(), library=requests):
     if not directory.is_dir():
         raise FileNotFoundError(f'{output_path} does not exist')
 
-    html_path = get_path(url, output_path)
-    download_html(url, html_path, library)
+    html_path = get_html_path(url, output_path)
+    download_html(url, html_path)
     download_resources(html_path, url)
 
     return html_path
