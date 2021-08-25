ecc95779bf7e5917da2d4d76d194fc45e79a52e5
denis <yudaev1@gmail.com>
2021-08-25 14:23:39 +0300

    Update resource_downloader.py

--- page_loader/resource_downloader.py
+++ page_loader/resource_downloader.py
@@ -6,43 +6,40 @@ import logging
 import requests
 
 
-TAGS_AND_ATTRIBUTES = (
+TAGS_AND_ATTRIBUTES_PAIRS = (
     ('img', 'src'), ('link', 'href'), ('script', 'src'),
 )
 
 
 @logging_info('Getting input data')
-def get_resources_data(file):
+def get_resources(file):
 
     # get pairs of links(list) and tag
-    resources_data = []
-    for tag, attr in TAGS_AND_ATTRIBUTES:
+    resources = []
+    for tag, attr in TAGS_AND_ATTRIBUTES_PAIRS:
         links = file.find_all(tag)
-        resources_data.append((links, attr))
-    return resources_data
+        resources.append((links, attr))
+    return resources
 
 
-def download_resource(url, resource_data, dir_path):
-    links, attr = resource_data
+def format_resource(url, resource, dir_path):
+    links, attr = resource
     parsed_url = urlparse(url)
     netloc = parsed_url.netloc
-    domain_name = f'{parsed_url.scheme}://{netloc}'
     for link in links:
         source = link.get(attr)
         if not source:
             continue
         if urlparse(source).netloc and urlparse(source).netloc != netloc:
             continue
-        if urlparse(source).netloc == netloc:
-            source = urlparse(source).path
-        url = urljoin(domain_name, source)
-
-        file_path = get_path(url, dir_path, html=False)
-        logging.info('Requesting to %s', url)
-        response = requests.get(url, stream=True)
+        source = urlparse(source).path
+        full_url = urljoin(url + '/', source)
+        file_path = get_path(full_url, dir_path)
+        logging.info('Requesting to %s', full_url)
+        response = requests.get(full_url, stream=True)
         response.raise_for_status()
-        download_file(file_path, url, response)
+        download_file(file_path, full_url, response)
 
         dir_name = split(dir_path)[1]
-        source_path = get_path(url, dir_name, html=False)
+        source_path = get_path(full_url, dir_name)
         link[attr] = source_path
