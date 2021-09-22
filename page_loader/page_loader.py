48b71d44cf03a686cc197f2c9578875898472d98
denis <yudaev1@gmail.com>
2021-09-22 17:12:31 +0300

    Refactor

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,8 +1,7 @@
-from pathlib import Path
-from page_loader.common import get_path, get_full_name
+from page_loader.support_functions import get_path, get_full_name
 from page_loader.logger import logging_info
 from page_loader.resource_downloader \
-    import get_resources, format_resource, save_resource
+    import walk_resources, save_resource
 from bs4 import BeautifulSoup
 import os
 import requests
@@ -12,7 +11,7 @@ import logging
 @logging_info('Creating download directory')
 def create_download_dir(dir_path):
     if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+        os.mkdir(dir_path)
 
 
 @logging_info('Parsing html')
@@ -22,34 +21,31 @@ def parse_html(html_file):
 
 
 def write_html(parsed_html, html_path):
-    logging.info(str(parsed_html))
+    logging.debug(str(parsed_html))
     with open(html_path, 'w') as file:
         file.write(parsed_html)
 
 
 def download(url, output_path=os.getcwd()):
 
-    directory = Path(output_path)
-    if not directory.is_dir() and not directory.is_file():
-        raise FileNotFoundError(f'Directory {output_path} does not exist')
-
-    html_name = get_full_name(url)
-    html_path = get_path(html_name, output_path, type='html')
-    download_dir_path = get_path(html_name, output_path, type='dir')
+    html_name = get_full_name(url, type='html')
+    download_dir_name = get_full_name(url, type='dir')
+    html_path = get_path(html_name, output_path)
+    download_dir_path = get_path(download_dir_name, output_path,)
     response = requests.get(url, stream=True)
     response.raise_for_status()
     html_file = response.content
     create_download_dir(download_dir_path)
 
-    logging.debug('Parsing html')
+    logging.info('Parsing html')
     parsed_html = parse_html(html_file)
 
-    resources = get_resources(parsed_html)
-    for resource in resources:
-        save_resource(url, resource, download_dir_path)
-        format_resource(url, resource, download_dir_path)
+    files_for_download = walk_resources(url, parsed_html, download_dir_path)
+    save_resource(files_for_download, download_dir_path)
 
     logging.info('Writing html')
     write_html(parsed_html.prettify(formatter="html5"), html_path)
 
     return html_path
+
+# 3. C тестами разобраться.
--- page_loader/resource_downloader.py
+++ page_loader/resource_downloader.py
@@ -1,61 +1,44 @@
-from page_loader.logger import logging_info
 from urllib.parse import urlparse, urljoin
 from os.path import split
-from page_loader.common import write_file, get_path, get_full_name
+from page_loader.support_functions import write_file, get_path, get_full_name
 import logging
 import requests
 
 
-TAGS_AND_ATTRIBUTES_PAIRS = (
+TAG_ATTRIBUTES = (
     ('img', 'src'), ('link', 'href'), ('script', 'src'),
 )
 
 
-@logging_info('Getting input data')
-def get_resources(file):
-    resources = []
-    for tag, attr in TAGS_AND_ATTRIBUTES_PAIRS:
-        links = file.find_all(tag)
-        resources.append((links, attr))
-    return resources
-
-
-def format_resource(url, resource, dir_path):
-    links, attr = resource
+def walk_resources(url, parsed_html, dir_path):
     parsed_url = urlparse(url)
     netloc = parsed_url.netloc
-    for link in links:
-        source = link.get(attr)
-        if not source:
-            continue
-        if urlparse(source).netloc and urlparse(source).netloc != netloc:
-            continue
-        source = urlparse(source).path
-        full_url = urljoin(url + '/', source)
-        file_name = get_full_name(full_url)
-        logging.info('Requesting to %s', full_url)
+    files_for_download = []
+    for tag, attr in TAG_ATTRIBUTES:
+        links = parsed_html.find_all(tag)
+        for link in links:
+            source = link.get(attr)
+            if not source:
+                continue
+            if urlparse(source).netloc and urlparse(source).netloc != netloc:
+                continue
+            source = urlparse(source).path
+            full_url = urljoin(url + '/', source)
+            file_name = get_full_name(full_url)
+            logging.info('Requesting to %s', full_url)
+            dir_name = split(dir_path)[1]
+            source_path = get_path(file_name, dir_name)
+            link[attr] = source_path
 
-        dir_name = split(dir_path)[1]
-        source_path = get_path(file_name, dir_name)
-        link[attr] = source_path
+            files_for_download.append((file_name, full_url))
+    return files_for_download
 
 
-def save_resource(url, resource, dir_path):
-    links, attr = resource
-    parsed_url = urlparse(url)
-    netloc = parsed_url.netloc
-    for link in links:
-        source = link.get(attr)
-        if not source:
-            continue
-        if urlparse(source).netloc and urlparse(source).netloc != netloc:
-            continue
-        source = urlparse(source).path
-        full_url = urljoin(url + '/', source)
-        file_name = get_full_name(full_url)
+def save_resource(files, dir_path):
+    for file in files:
+        file_name, full_url = file
         file_path = get_path(file_name, dir_path)
         logging.info('Requesting to %s', full_url)
-
         response = requests.get(full_url, stream=True)
         response.raise_for_status()
         write_file(file_path, full_url, response)
