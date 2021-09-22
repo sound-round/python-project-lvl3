13cea11e4c1a6ff553b79b2c952ebc696353d1e7
denis <yudaev1@gmail.com>
2021-09-22 13:09:56 +0300

    Refactor support_functions.py'

--- page_loader/support_functions.py
+++ page_loader/support_functions.py
@@ -13,23 +13,23 @@ DIR_ENDING = '_files'
 
 def get_full_name(url):
     parsed_url = urlparse(url)
-    return f'{parsed_url.netloc}{parsed_url.path}'
+    path, ext = splitext(parsed_url.path)
+    return f'{format_name(parsed_url.netloc + path).strip("-")}{ext}'  # TODO splitext на последнюю часть
 
 
 def get_path(full_file_name, output_path, type=''):
+    path = join(output_path, full_file_name)
     if type == 'dir':
-        return join(output_path, format_name(full_file_name)) + DIR_ENDING
+        return path + DIR_ENDING
 
     file_name, file_ext = splitext(full_file_name)
     if not file_ext or type == 'html':
-        return join(output_path, format_name(full_file_name)) + HTML_EXT
+        return path + HTML_EXT
 
-    return join(output_path, format_name(file_name)) + file_ext
+    return path
 
 
 def format_name(file_name, replacer='-'):
-    # if file_name[-1] == '/':
-    #     file_name = file_name[:-1]
     formatted_file_name = re.sub(FORBIDDEN_CHARS, replacer, file_name).lower()
     return formatted_file_name
 
@@ -40,7 +40,7 @@ def write_file(file_path, url, response):
         total_size = int(response.headers.get('Content-Length'))
     with open(file_path, 'wb') as file:
         with ChargingBar(url, max=total_size / CHUNK_SIZE) as bar:
-            logging.debug('Downloading chunks')
+            logging.info('Downloading chunks')
             for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
                 file.write(chunk)
                 bar.next()
